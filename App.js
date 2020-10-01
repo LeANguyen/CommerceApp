a02fd52db0c5613935b7d56c5fe91ed8b8f45d78
LeANguyen <scorcher159@gmail.com>
2020-10-01 15:57:49 +0700

    add welcome view

--- App.js
+++ App.js
@@ -19,95 +19,11 @@ import {
   useDimensions,
   useDeviceOrientation
 } from "@react-native-community/hooks";
-
-// export default class Apple extends React.Component {
-//   constructor() {
-//     super();
-
-//     this.state = { orientation: "" };
-//   }
-// }
+import WelcomeView from "./app/views/WelcomeView";
 
 export default function App() {
-  const isPortrait = () => {
-    const dim = Dimensions.get("screen");
-    return dim.height >= dim.width;
-  };
-
-  Dimensions.addEventListener("change", () => {
-    // this.setState({
-    //     orientation: isPortrait() ? "portrait" : "landscape"
-    //   });
-    // console.log("BFF");
-    console.log();
-  });
-
-  // console.log(Dimensions.get("screen"));
-  // console.log(useDimensions());
-  // console.log(useDeviceOrientation());
-
-  // const orientation = useDeviceOrientation();
-  // console.log(orientation);
-  // console.log(orientation.landscape);
-
-  const handlePress1 = () => {
-    console.log("Pressed 1");
-    console.log("Pressed 1.5");
-  };
-
-  const handlePress2 = () => {
-    console.log("Pressed 2");
-    console.log("Pressed 2.5");
-  };
-
-  const handlePress3 = () => {
-    Alert.alert("My title", "My messagee", [
-      { text: "Yes", onPress: handlePress1 },
-      { text: "No" }
-    ]);
-  };
-
-  const image1URI =
-    "https://i.ebayimg.com/images/g/CUQAAOSw-nZTmzJB/s-l300.jpg";
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          width: "100%",
-          // height: isPortrait() ? "30%" : "100%",
-          flex: 1,
-          backgroundColor: "dodgerblue"
-        }}
-      ></View>
-
-      <Text onPress={handlePress1}>
-        Open up App.js to start working on your apple!
-      </Text>
-
-      <TouchableOpacity onPress={handlePress2}>
-        <Image source={require("./app/assets/icon.png")}></Image>
-      </TouchableOpacity>
-
-      <Image
-        blurRadius={2}
-        fadeDuration={3000}
-        source={{
-          width: 200,
-          height: 200,
-          uri: image1URI
-        }}
-      ></Image>
-
-      <Button
-        title={"KISS ME"}
-        color={"purple"}
-        onPress={handlePress3}
-      ></Button>
-
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  console.log("App started");
+  return <WelcomeView />;
 }
 
 const styles = StyleSheet.create({
--- app/views/WelcomeView.js
+++ app/views/WelcomeView.js
@@ -1,5 +1,55 @@
-// import React from 'react';
-// import { storiesOf } from '@storybook/react-native';
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
 
-// storiesOf('', module)
-//     .add('default', () => );
+function WelcomeView(prop) {
+  return (
+    <View style={styles.viewContainer}>
+      <ImageBackground
+        source={require("../assets/welcome_background.jpg")}
+        style={styles.background}
+      >
+        <Image
+          style={styles.logoImage}
+          source={require("../assets/logo.png")}
+        ></Image>
+        <Text style={styles.slogan}>Sell What You Don't Need</Text>
+      </ImageBackground>
+      <View style={styles.loginButton}></View>
+      <View style={styles.regButton}></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1
+  },
+  background: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center"
+  },
+  loginButton: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "orange"
+  },
+  regButton: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "black"
+  },
+  logoImage: {
+    width: 100,
+    height: 100,
+    top: 75
+  },
+  slogan: {
+    color: "white",
+    top: 75,
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
+
+export default WelcomeView;
